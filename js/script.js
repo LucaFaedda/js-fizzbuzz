85a8abe0c4570f4d0567841961e92dbcb94fa232
LucaFaedda <lucakarl91@gmail.com>
2022-11-29 15:58:52 +0100

    completato es principale

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert('ciao')
+
+for (i = 1; i<=100 ; i++ ){
+
+    let number;
+
+    if(i%3 == 0 && i%5 ==0){
+        number = 'FizzBuzz'
+        console.log(number)
+    }
+    else if(i%5 == 0){
+        number = 'Buzz'
+        console.log(number)
+    }
+    else if(i%3 == 0 ){
+        number = 'Fizz'
+        console.log(number)
+    }
+    else{
+        number = i
+        console.log(number)
+    }
+}
