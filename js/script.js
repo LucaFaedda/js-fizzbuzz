8c5e48e2e4e22c9c5b333644aed8cd0f4dc111aa
LucaFaedda <lucakarl91@gmail.com>
2022-11-29 17:04:57 +0100

    completato quasi bonus numero2

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 const container = document.querySelector('.container')
-
+let num = 1
 
 for (i = 1; i<=100 ; i++ ){
 
     let number;
 
     if(i%3 == 0 && i%5 ==0){
-        number = '<div class = "box box-red"> ${FrizzBuzz}</div>'
-        
+        number = '<div class = "box box-red"> FrizzBuzz </div>'
     }
     else if(i%5 == 0){
         number = 'Buzz'
-        console.log(number)
+        number = '<div class = "box box-green"> Buzz </div>'
     }
     else if(i%3 == 0 ){
         number = 'Fizz'
-        console.log(number)
+        number = '<div class = "box box-yellow"> Frizz </div>'
     }
     else{
         number = i
-        console.log(number)
+        number = '<div class = "box box-orange">${i} </div>'
     }
+    container.innerHTML += number
 }
