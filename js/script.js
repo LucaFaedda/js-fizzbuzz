9204942077ff27e9b8a9b12040e7b911912de92b
LucaFaedda <lucakarl91@gmail.com>
2022-11-29 16:46:08 +0100

    aggiunto regole css

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
+const container = document.querySelector('.container')
+
 
 for (i = 1; i<=100 ; i++ ){
 
     let number;
 
     if(i%3 == 0 && i%5 ==0){
-        number = 'FizzBuzz'
-        console.log(number)
-        document.getElementById('box').innerHTML = 'FizzBuzz'
+        number = '<div class = "box box-red"> ${FrizzBuzz}</div>'
+        
     }
     else if(i%5 == 0){
         number = 'Buzz'
