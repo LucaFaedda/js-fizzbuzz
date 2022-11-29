631b3e38979c6f26fb97fecbe1b1c150462a7f26
LucaFaedda <lucakarl91@gmail.com>
2022-11-29 17:16:39 +0100

    completato bonus

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ for (i = 1; i<=100 ; i++ ){
     let number;
 
     if(i%3 == 0 && i%5 ==0){
-        number = '<div class = "box box-red"> FrizzBuzz </div>'
+        
+        number = `<div class = "box box-red"> ${number} </div>`
     }
     else if(i%5 == 0){
         number = 'Buzz'
-        number = '<div class = "box box-green"> Buzz </div>'
+        number = `<div class = "box box-green"> ${number} </div>`
     }
     else if(i%3 == 0 ){
         number = 'Fizz'
-        number = '<div class = "box box-yellow"> Frizz </div>'
+        number = `<div class = "box box-yellow">  ${number} </div>`
     }
     else{
-        number = i
-        number = '<div class = "box box-orange">${i} </div>'
+        number = `<div class = "box box-orange">${i} </div>`
     }
     container.innerHTML += number
 }
