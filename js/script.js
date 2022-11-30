2a6a82056293d84d03e39515eb6496a2e9365e84
LucaFaedda <lucakarl91@gmail.com>
2022-11-30 10:05:33 +0100

    aggiunto commento

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ for (i = 1; i<=100 ; i++ ){
         number = "<div class = 'box box-red'>""+number+" "</div>"`
         number = '<div class = "box box-red">'+number+' '</div>'
         */
-       // inoltre potevo al posto di mettere solo number = potevo scrivere dirrettamente container.innerHTML = `<div class = "box box-red"> ${number} </div>`
+       // inoltre potevo al posto di mettere solo number = potevo scrivere dirrettamente container.innerHTML = `<div class = "box box-red"> ${number} </div>` facendo così abbiamo una variabile in meno perchè non c'è bisogno di dichiarare number fuori dall'if. Per contro ripetiamo più volte container.innerHTML
 
         // Sbagliato mettere ` avevo messo ' 
         // potevo lasciare i%3 == 0 aggiungendoci && i%5 !=0  e al secondo else if lasciare i%3 == 0 && i%5 ==0 
