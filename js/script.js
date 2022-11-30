29594bf93c613335fd12de42d863d28e58f9b02a
LucaFaedda <lucakarl91@gmail.com>
2022-11-30 09:59:05 +0100

    commenti

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@ for (i = 1; i<=100 ; i++ ){
     if(i%3 == 0 && i%5 ==0){
         number = 'FrizzBuzz'
         number = `<div class = "box box-red"> ${number} </div>`
+        /* altri metodi:
+        number = "<div class = 'box box-red'>""+number+" "</div>"`
+        number = '<div class = "box box-red">'+number+' '</div>'
+        */
+       // inoltre potevo al posto di mettere solo number = potevo scrivere dirrettamente container.innerHTML = `<div class = "box box-red"> ${number} </div>`
+
         // Sbagliato mettere ` avevo messo ' 
         // potevo lasciare i%3 == 0 aggiungendoci && i%5 !=0  e al secondo else if lasciare i%3 == 0 && i%5 ==0 
     }
